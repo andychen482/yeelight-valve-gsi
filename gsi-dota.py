d39d3066f3c15d2c5112af9c199cdc81e3fde188
David Ramiro <david@kore.cc>
2018-02-20 10:29:44 +0100

    trim code for easier testing
    Removing multiple lamp support for easier testing (for now).
    Fixed GSI cfg location.

--- gsi-dota.py
+++ gsi-dota.py
@@ -29,40 +29,16 @@ class MyRequestHandler(BaseHTTPRequestHandler):
         if daytime != self.server.daytime:
             self.server.daytime = daytime
             print('Day time toggled')
-            
-            if bulb1 != '':
-                bulb = Bulb(bulb1)
-                bulb.set_rgb(153, 102, 255)
-                bulb.set_brightness(100)
-                
-            if bulb2 != '':
-                bulb = Bulb(bulb2)
-                bulb.set_rgb(153, 102, 255)
-                bulb.set_brightness(100)
-                
-            if bulb3 != '':
-                bulb = Bulb(bulb3)
-                bulb.set_rgb(153, 102, 255)
-                bulb.set_brightness(100)
+
+            bulb.set_rgb(153, 102, 255)
+            bulb.set_brightness(100)
                 
         if nighttime != self.server.nighttime:
             self.server.nighttime = nighttime
             print('Night time toggled')
-            
-            if bulb1 != '':
-                bulb = Bulb(bulb1)
-                bulb.set_rgb(255, 0, 0)
-                bulb.set_brightness(20)
-                
-            if bulb2 != '':
-                bulb = Bulb(bulb2)
-                bulb.set_rgb(255, 0, 0)
-                bulb.set_brightness(20)
-                
-            if bulb3 != '':
-                bulb = Bulb(bulb3)
-                bulb.set_rgb(255, 0, 0)
-                bulb.set_brightness(20)        
+
+            bulb.set_rgb(255, 0, 0)
+            bulb.set_brightness(20)
             
     def get_daytime(self, payload):
         if 'Map' in payload and 'IsDayTime' in payload['Map']:
@@ -85,32 +61,14 @@ server.init_state()
 config = configparser.ConfigParser()
 config.read('config.ini')
 bulb1 = config.get('lamps','ip1')
-bulb2 = config.get('lamps','ip2')
-bulb3 = config.get('lamps','ip3')
 
-if bulb1 != '':
-    print('Initializing first Yeelight')
-    bulb = Bulb(bulb1)
-    bulb.turn_on()
-    bulb.start_music()
-    bulb.set_rgb(0, 0, 255)
-    bulb.set_brightness(100)
 
-if bulb2 != '':
-    print('Initializing second Yeelight')
-    bulb = Bulb(bulb2)
-    bulb.turn_on()
-    bulb.start_music()
-    bulb.set_rgb(0, 0, 255)
-    bulb.set_brightness(100)
-
-if bulb3 != '':
-    print('Initializing third Yeelight')
-    bulb = Bulb(bulb3)
-    bulb.turn_on()
-    bulb.start_music()
-    bulb.set_rgb(0, 0, 255)
-    bulb.set_brightness(100)
+print('Initializing Yeelight')
+bulb = Bulb(bulb1)
+bulb.turn_on()
+bulb.start_music()
+bulb.set_rgb(0, 0, 255)
+bulb.set_brightness(100)
 
 print(time.asctime(), '-', 'GSI running - CTRL+C to stop')
 try:
@@ -119,17 +77,6 @@ except (KeyboardInterrupt, SystemExit):
     pass
 
 server.server_close()
-
-if bulb1 != '':
-    bulb = Bulb(bulb1)
-    bulb.stop_music
-    
-if bulb2 != '':
-    bulb = Bulb(bulb2)
-    bulb.stop_music
-    
-if bulb3 != '':
-    bulb = Bulb(bulb3)
-    bulb.stop_music
+bulb.stop_music
 
 print(time.asctime(), '-', 'GSI server stopped')
