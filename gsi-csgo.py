2dd50fff2d7474cfc94c96c1ecb76eac2c59b2c3
David Ramiro <david@kore.cc>
2018-02-23 18:47:26 +0100

    tweak hp processing
    Using health percentage instead of static hp value for dota 2.
    
    Not needed for csgo as health is always 0 to 100.

--- gsi-csgo.py
+++ gsi-csgo.py
@@ -10,16 +10,13 @@ class MyServer(HTTPServer):
     def init_state(self):
         self.round_phase = None
         self.round_bomb = None
-        self.player_state = None
         self.player_health = None
 
 class MyRequestHandler(BaseHTTPRequestHandler):
     def do_POST(self):
         length = int(self.headers['Content-Length'])
         body = self.rfile.read(length).decode('utf-8')
-
         self.parse_payload(json.loads(body))
-
         self.send_header('Content-type', 'text/html')
         self.send_response(200)
         self.end_headers()
@@ -27,114 +24,106 @@ class MyRequestHandler(BaseHTTPRequestHandler):
     def parse_payload(self, payload):
         round_phase = self.get_round_phase(payload)
         round_bomb = self.get_round_bomb(payload)
-        player_state = self.get_player_state(payload)
         player_health = self.get_player_health(payload)
 
-        if bulb1 != '':
-            if round_bomb != self.server.round_bomb:
-                self.server.round_bomb = round_bomb
-                bulb = Bulb(bulb1)
-                print('Lamp one - changed bomb status: %s' % round_bomb)
-                if 'exploded' in round_bomb:
-                    bulb.set_rgb(0, 255, 0)  
-                if 'planted' in round_bomb:
+        if round_bomb != self.server.round_bomb:
+            self.server.round_bomb = round_bomb
+            print('changed bomb status: %s' % round_bomb)
+            if 'exploded' in round_bomb:
+                if bulb1 != '':
+                    bulb = Bulb(bulb1)
+                    bulb.set_rgb(0, 255, 0)
+                if bulb2 != '':
+                    bulb = Bulb(bulb2)
+                    bulb.set_rgb(0, 255, 0)
+                if bulb3 != '':
+                    bulb = Bulb(bulb3)
+                    bulb.set_rgb(0, 255, 0)
+            if 'planted' in round_bomb:
+                if bulb1 != '':
+                    bulb = Bulb(bulb1)
                     bulb.set_rgb(255, 0, 0)
-                if 'defused' in round_bomb:
+                if bulb2 != '':
+                    bulb = Bulb(bulb2)
+                    bulb.set_rgb(255, 0, 0)
+                if bulb3 != '':
+                    bulb = Bulb(bulb3)
+                    bulb.set_rgb(255, 0, 0)
+            if 'defused' in round_bomb:
+                if bulb1 != '':
+                    bulb = Bulb(bulb1)
+                    bulb.set_rgb(0, 0, 255)
+                if bulb2 != '':
+                    bulb = Bulb(bulb2)
+                    bulb.set_rgb(0, 0, 255)
+                if bulb3 != '':
+                    bulb = Bulb(bulb3)
                     bulb.set_rgb(0, 0, 255)
 
-        if bulb2 != '':
-            if round_bomb != self.server.round_bomb:
-                self.server.round_bomb = round_bomb
-                bulb = Bulb(bulb2)
-                print('Lamp two - changed bomb status: %s' % round_bomb)
-                if 'exploded' in round_bomb:
-                    bulb.set_rgb(0, 255, 0)  
-                if 'planted' in round_bomb:
+        if round_phase != self.server.round_phase:
+            self.server.round_phase = round_phase
+            print('new round phase: %s' % round_phase)
+            if 'over' in round_phase:
+                if bulb1 != '':
+                    bulb = Bulb(bulb1)
                     bulb.set_rgb(255, 0, 0)
-                if 'defused' in round_bomb:
-                    bulb.set_rgb(0, 0, 255)
-
-        if bulb3 != '':
-            if round_bomb != self.server.round_bomb:
-                self.server.round_bomb = round_bomb
-                bulb = Bulb(bulb3)
-                print('Lamp three - changed bomb status: %s' % round_bomb)
-                if 'exploded' in round_bomb:
-                    bulb.set_rgb(0, 255, 0)  
-                if 'planted' in round_bomb:
+                if bulb2 != '':
+                    bulb = Bulb(bulb2)
                     bulb.set_rgb(255, 0, 0)
-                if 'defused' in round_bomb:
-                    bulb.set_rgb(0, 0, 255)
-                
-        if bulb1 != '':        
-            if round_phase != self.server.round_phase:
-                self.server.round_phase = round_phase
-                bulb = Bulb(bulb1)
-                print('Lamp one - new round phase: %s' % round_phase)
-                if 'over' in round_phase:
+                if bulb3 != '':
+                    bulb = Bulb(bulb3)
                     bulb.set_rgb(255, 0, 0)
-                if 'live' in round_phase:
+            if 'live' in round_phase:
+                if bulb1 != '':
+                    bulb = Bulb(bulb1)
                     bulb.set_rgb(153, 102, 255)
-                if 'freezetime' in round_phase:
-                    bulb.set_rgb(0, 0, 255)
-                
-        if bulb2 != '':                
-            if round_phase != self.server.round_phase:
-                self.server.round_phase = round_phase
-                bulb = Bulb(bulb2)
-                print('Lamp two - new round phase: %s' % round_phase)
-                if 'over' in round_phase:
-                    bulb.set_rgb(255, 0, 0)
-                if 'live' in round_phase:
+                if bulb2 != '':
+                    bulb = Bulb(bulb2)
                     bulb.set_rgb(153, 102, 255)
-                if 'freezetime' in round_phase:
-                    bulb.set_rgb(0, 0, 255)
-                
-        if bulb3 != '':
-            if round_phase != self.server.round_phase:
-                self.server.round_phase = round_phase
-                bulb = Bulb(bulb3)
-                print('Lamp three - new round phase: %s' % round_phase)
-                if 'over' in round_phase:
-                    bulb.set_rgb(255, 0, 0)
-                if 'live' in round_phase:
+                if bulb3bulb3 != '':
+                    bulb = Bulb(bulb3)
                     bulb.set_rgb(153, 102, 255)
-                if 'freezetime' in round_phase:
+            if 'freezetime' in round_phase:
+                if bulb1 != '':
+                    bulb = Bulb(bulb1)
                     bulb.set_rgb(0, 0, 255)
-                
-        if player_state != self.server.player_state:
-            self.server.player_state = player_state
-            print('new player state: %s' % player_state)
+                if bulb2 != '':
+                    bulb = Bulb(bulb2)
+                    bulb.set_rgb(0, 0, 255)
+                if bulb3 != '':
+                    bulb = Bulb(bulb3)
+                    bulb.set_rgb(0, 0, 255)
 
         if player_health != self.server.player_health:
             self.server.player_health = player_health
             print('player health: %s' % player_health)
-        
+            if bulb1 != '':
+                bulb = Bulb(bulb1)
+                bulb.set_rgb(0xff0000 * player_health)
+            if bulb2 != '':
+                bulb = Bulb(bulb2)
+                bulb.set_rgb(0xff0000 * player_health)
+            if bulb3 != '':
+                bulb = Bulb(bulb3)
+                bulb.set_rgb(0xff0000 * player_health)
+
     def get_round_phase(self, payload):
         if usePhase == True:
             if 'round' in payload and 'phase' in payload['round']:
                 return payload['round']['phase']
             else:
                 return None
-            
+
     def get_round_bomb(self, payload):
         if useBomb == True:
             if 'round' in payload and 'bomb' in payload['round']:
                 return payload['round']['bomb']
             else:
                 return None
-            
-    def get_player_state(self, payload):
-        if 'player' in payload and 'state' in payload['player']:
-            return payload['player']['state']
-            print('new payload player & state')
-        else:
-            return None
-            
+
     def get_player_health(self, payload):
-        if 'health' in payload:
-            return payload['health']
-            print('new payload player & state')
+        if 'player' in payload and 'state' in payload['player']:
+            return payload['player']['state']['health']
         else:
             return None
 
@@ -188,11 +177,9 @@ server.server_close()
 if bulb1 != '':
     bulb = Bulb(bulb1)
     bulb.stop_music
-    
 if bulb2 != '':
     bulb = Bulb(bulb2)
     bulb.stop_music
-    
 if bulb3 != '':
     bulb = Bulb(bulb3)
     bulb.stop_music
--- gsi-dota.py
+++ gsi-dota.py
@@ -14,33 +14,25 @@ class MyRequestHandler(BaseHTTPRequestHandler):
     def do_POST(self):
         length = int(self.headers['Content-Length'])
         body = self.rfile.read(length).decode('utf-8')
-
         self.parse_payload(json.loads(body))
-
         self.send_header('Content-type', 'text/html')
         self.send_response(200)
         self.end_headers()
 
     def parse_payload(self, payload):
         health = self.get_health(payload)
-        
+
         if health != self.server.health:
             self.server.health = health
             print('health state changed to %s' % health)
-            if health <= 500:
-                bulb.set_rgb(255, 0, 0)
-                bulb.set_brightness(100)
-            if health > 500:    
-                bulb.set_rgb(0, 255, 0)
-                bulb.set_brightness(100)
-                
-            
+            bulb.set_rgb(0xff0000 * health)
+
     def get_health(self, payload):
-        if 'hero' in payload and 'health' in payload['hero']:
-            return payload['hero']['health']
+        if 'hero' in payload and 'health_percent' in payload['hero']:
+            return payload['hero']['health_percent']
         else:
             return None
-    
+
     def log_message(self, format, *args):
         return
 
@@ -50,7 +42,6 @@ server.init_state()
 config = configparser.ConfigParser()
 config.read('config.ini')
 bulb1 = config.get('lamps','ip1')
-
 
 print('Initializing Yeelight')
 bulb = Bulb(bulb1)
